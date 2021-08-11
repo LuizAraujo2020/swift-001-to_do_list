74c3549d249469685db6dc602a560b59edf33ac2
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-11 11:35:55 -0300

    Initial Commit

---
+++ swift-001-to_do_list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-001-to_do_list
+//
+//  Created by Luiz Carlos da Silva Araujo on 11/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
